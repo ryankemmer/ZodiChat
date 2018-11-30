d3c4b174526d737cc087f20d13e1b4e8048f1be4
Ryan Kemmer <ryankemmer@ryans-mbp-8.3521.dhcp.asu.edu>
2018-11-29 17:44:30 -0700

    Cleared db and profile pics

--- index.js
+++ index.js
@@ -172,85 +172,90 @@ app.post('/signup', upload.single('avatar'), bodyParse, async (req, res) => {
     var zod_signs = ["Capricorn" , "Aquarius", "Pisces", "Aries",
     "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio",
     "Sagittarius"];
-    var dateParts = userBirthday.split("-");
-    var day = dateParts[2];
+    var dateParts = req.body.birthday.split("-");
     var month = dateParts[1];
+    var day = dateParts[2];
+    var zodiacSign = "";
+
+    month = parseInt(month,10);
+    day = parseInt(day,10);
 
     switch(month){
-        case 00: {//January
-                 if(day < 20)
-                     zodiacSign = zod_signs[0];
-                 else
-                     zodiacSign = zod_signs[1];
-                }break;
-        case 01: {//February
-                 if(day < 19)
-                     zodiacSign = zod_signs[1];
-                 else
-                     zodiacSign = zod_signs[2];
-                }break;
-        case 02: {//March
-                 if(day < 21)
-                     zodiacSign = zod_signs[2];
-                 else
-                     zodiacSign = zod_signs[3];
-                }break;
-        case 03: {//April
-                 if(day < 20)
-                     zodiacSign = zod_signs[3];
-                 else
-                     zodiacSign = zod_signs[4];
-                }break;
-        case 04: {//May
-                 if(day < 21)
-                     zodiacSign = zod_signs[4];
-                 else
-                     zodiacSign = zod_signs[5];
-                }break;
-        case 05: {//June
-                 if(day < 21)
-                     zodiacSign = zod_signs[5];
-                 else
-                     zodiacSign = zod_signs[6];
-                }break;
-        case 06: {//July
-                 if(day < 23)
-                     zodiacSign = zod_signs[6];
-                 else
-                     zodiacSign = zod_signs[7];
-                }break;
-         case 07: {//August
-                 if(day < 23)
-                     zodiacSign = zod_signs[7];
-                 else
-                     zodiacSign = zod_signs[8];
-                }break;
-        case 08: {//September
-                 if(day < 23)
-                     zodiacSign = zod_signs[8];
-                 else
-                     zodiacSign = zod_signs[9];
-                }break;
-        case 09: {//October
-                 if(day < 23)
-                     zodiacSign = zod_signs[9];
-                 else
-                     zodiacSign = zod_signs[10];
-                }break;
-        case 10: {//November
-                 if(day < 22)
-                     zodiacSign = zod_signs[10];
-                 else
-                     zodiacSign = zod_signs[11];
-                }break;
-        case 11: {//December
-                 if(day < 22)
-                     zodiacSign = zod_signs[11];
-                 else
-                     zodiacSign = zod_signs[0];
-                }break;
-     }
+	case 0: {//January
+			 if(day < 20)
+		 		zodiacSign = zod_signs[0];
+			 else
+		 		zodiacSign = zod_signs[1];
+		    }break;
+	case 1: {//February
+			 if(day < 19)
+		 		zodiacSign = zod_signs[1];
+			 else
+		 		zodiacSign = zod_signs[2];
+			}break;
+	case 2: {//March
+			 if(day < 21)
+			 	zodiacSign = zod_signs[2];
+			 else
+			 	zodiacSign = zod_signs[3];
+			}break;
+	case 3: {//April
+			 if(day < 20)
+		 		zodiacSign = zod_signs[3];
+			 else
+		 		zodiacSign = zod_signs[4];
+			}break;
+	case 4: {//May
+			 if(day < 21)
+		 		zodiacSign = zod_signs[4];
+			 else
+		 		zodiacSign = zod_signs[5];
+			}break;
+	case 5: {//June
+			 if(day < 21)
+		 		zodiacSign = zod_signs[5];
+			 else
+		 		zodiacSign = zod_signs[6];
+			}break;
+	case 6: {//July
+			 if(day < 23)
+		 		zodiacSign = zod_signs[6];
+			 else
+		 		zodiacSign = zod_signs[7];
+			}break;
+ 	case 7: {//August
+			 if(day < 23)
+		 		zodiacSign = zod_signs[7];
+			 else
+		 		zodiacSign = zod_signs[8];
+			}break;
+	case 8: {//September
+			 if(day < 23)
+		 		zodiacSign = zod_signs[8];
+			 else
+		 		zodiacSign = zod_signs[9];
+			}break;
+	case 9: {//October
+			 if(day < 23)
+		 		zodiacSign = zod_signs[9];
+			 else
+		 		zodiacSign = zod_signs[10];
+			}break;
+	case 10: {//November
+			 if(day < 22)
+		 		zodiacSign = zod_signs[10];
+			 else
+		 		zodiacSign = zod_signs[11];
+			}break;
+	case 11: {//December
+			 if(day < 22)
+		 		zodiacSign = zod_signs[11];
+			 else
+		 		zodiacSign = zod_signs[0];
+			}break;
+ }
         
+     console.log(zodiacSign);
 
     const result = await db.run(
         'INSERT INTO users(firstName,lastName,passwordHash,email,gender,birthday,created,profilepic,sign) VALUES (?,?,?,?,?,?,?,?,?);',
@@ -280,6 +285,7 @@ app.post('/signup', upload.single('avatar'), bodyParse, async (req, res) => {
     res.redirect('/home');
 */
 
+    res.redirect('/login');
 });
 
 
